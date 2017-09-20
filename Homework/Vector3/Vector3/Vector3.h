db7c2296086c6e13b09ec06d9b7e38ff8bc87899
Sandra Alvarez <sandruskiag@gmail.com>
2017-09-20 08:42:38 +0200

    Vec3 notes

--- Homework/Vector3/Vector3/Vector3.h
+++ Homework/Vector3/Vector3/Vector3.h
@@ -8,22 +8,29 @@ public:
 
 public:
 	Vec3() {}
-	Vec3(const TYPE x, const TYPE y, const TYPE z);
+	Vec3(const TYPE& x, const TYPE& y, const TYPE& z);
 	Vec3(const Vec3& vec3);
 
 	Vec3 operator+(const Vec3& vec3) const;
 	Vec3 operator-(const Vec3& vec3) const;
 	void operator+=(const Vec3& vec3);
 	void operator-=(const Vec3& vec3);
-	Vec3 operator*(TYPE k);
-	void operator*=(TYPE k);
+	Vec3 operator*(const TYPE& k); //Encara que sigui un escalar, pot ser un escalar que vingui d'una variable, i no volem ni fer una còpia d'aquesta variable ni modificar-la dins d'aquest mètode
+	void operator*=(const TYPE& k);
 	TYPE operator*(const Vec3& vec3);
+
+	/*
+	Falten mètodes:
+	-Distància entre 2 vectors.
+	-Si un vector és 0 -> bool is_zero();
+	-Operador d'assignació -> operator= 
+	*/
 
 	void zero();
 };
 
 template <class TYPE>
-Vec3<TYPE>::Vec3(const TYPE x, const TYPE y, const TYPE z) :x(x), y(y), z(z) {}
+Vec3<TYPE>::Vec3(const TYPE& x, const TYPE& y, const TYPE& z) :x(x), y(y), z(z) {}
 
 template <class TYPE>
 Vec3<TYPE>::Vec3(const Vec3& vec3) {
@@ -51,14 +58,17 @@ Vec3<TYPE> Vec3<TYPE>::operator-(const Vec3& vec3) const {
 	aux.y = y - vec3.y;
 	aux.z = z - vec3.z;
 
-	return aux;
+	return aux; //Implementar-ho tot directament al return, sense la necessitat de crear un Vec3.
 }
 
 template <class TYPE>
-void Vec3<TYPE>::operator+=(const Vec3& vec3) {
+void Vec3<TYPE>::operator+=(const Vec3& vec3) { //Norma de C: els operadors de suma, resta, etc. sempre han de retornar un resultat. El retorn no pot ser 'void'.
 	x += vec3.x;
 	y += vec3.y;
 	z += vec3.z;
+
+	//return *this;
+	//El retorn del mètode pot ser 'const Vec3<TYPE>'
 }
 
 template <class TYPE>
@@ -76,7 +86,7 @@ void Vec3<TYPE>::zero() {
 }
 
 template <class TYPE>
-Vec3<TYPE> Vec3<TYPE>::operator*(TYPE k) {
+Vec3<TYPE> Vec3<TYPE>::operator*(const TYPE& k) {
 	Vec3 aux;
 
 	aux.x = x * k;
@@ -87,7 +97,7 @@ Vec3<TYPE> Vec3<TYPE>::operator*(TYPE k) {
 }
 
 template <class TYPE>
-void Vec3<TYPE>::operator*=(TYPE k) {
+void Vec3<TYPE>::operator*=(const TYPE& k) {
 	x *= k;
 	y *= k;
 	z *= k;
@@ -97,9 +107,6 @@ template <class TYPE>
 TYPE Vec3<TYPE>::operator*(const Vec3& vec3) {
 	return (x * vec3.x) + (y * vec3.y) + (z * vec3.z);
 }
-
-
-
 
 template <class TYPE>
 bool operator==(const Vec3<TYPE>& vec3a, const Vec3<TYPE>& vec3b) {
